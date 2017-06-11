19454f171e2741af410fe552bfff06da146717be
ginpei <ginpei@gmail.com>
2017-06-11 16:04:51 -0700

    remove debug messages

--- src/TaskShow.vue
+++ src/TaskShow.vue
@@ -9,7 +9,6 @@ module.exports = {
 	data() {
 		const items = this.$store.state.items;
 		const id = this.$route.params.id;
-		console.debug(items.length, items.find(d=>d.id===id));
 		return items.find(d=>d.id===id);
 	},
 };
