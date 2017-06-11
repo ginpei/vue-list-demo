3a2a78438c5dd12d23f6e0c3dcbe65520649dc10
ginpei <ginpei@gmail.com>
2017-06-11 15:15:10 -0700

    not used

--- index.js
+++
@@ -1,9 +0,0 @@
-const Vue = require('vue');
-const App = require('./App.vue');
-const store = require('./store.js');
-
-new Vue({  // eslint-disable-line no-new
-	el: '#app',
-	store,
-	render: f=>f(App),
-});
