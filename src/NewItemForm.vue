dafe3be12e3217e8a49f0bbd42dee93d4a1c25be
ginpei <ginpei@gmail.com>
2017-06-11 14:24:55 -0700

    add

--- src/NewItemForm.vue
+++ src/NewItemForm.vue
@@ -1,6 +1,40 @@
+<style>
+.newItemForm {
+	margin-bottom: 1em;
+}
+</style>
+
 <template>
-	<form @submit="onsubmit">
-		<input type="text" placeholder="ex) Buy milk" />
+	<form @submit="onsubmit" class="newItemForm">
+		<input v-model="name" type="text" placeholder="ex) Buy milk" />
 		<button>Create</button>
 	</form>
 </template>
+
+<script>
+module.exports = {
+	data() {
+		return {
+			name: '',
+		};
+	},
+
+	methods: {
+		onsubmit(event) {
+			event.preventDefault();
+
+			// do nothing if empty
+			if (!this.name) {
+				return;
+			}
+
+			const data = {
+				name: this.name,
+			};
+			this.$store.dispatch('createNewItem', data);
+
+			this.name = '';
+		},
+	},
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -24,13 +24,17 @@ module.exports = new Vuex.Store({
 			state.items = value;
 		},
 
-		INIT_CHECKED( state, value) {
-			state.checked = value;
+		INIT_CHECKED( state, items) {
+			state.checked = state.items.reduce((r,d)=>(r[d.id]=false,r),{});
 		},
 
 		TOGGLE_CHECKED_ALL(state, value) {
 			const o = state.checked;
 			Object.keys(o).forEach(d=>o[d]=value);
+		},
+
+		ADD_ITEM(state, value) {
+			state.items.push(value);
 		},
 	},
 
@@ -38,12 +42,18 @@ module.exports = new Vuex.Store({
 		setItems({ commit }, items) {
 			commit('SET_ITEMS', items);
 
-			const checked = items.reduce((r,d)=>(r[d.id]=false,r),{});
-			commit('INIT_CHECKED', checked);
+			commit('INIT_CHECKED');
 		},
 
 		toggleCheckAll({ commit }, checked) {
 			commit('TOGGLE_CHECKED_ALL', checked);
+		},
+
+		createNewItem({ commit }, data) {
+			data.id = 'item-' + Date.now();
+			commit('ADD_ITEM', data);
+
+			commit('INIT_CHECKED');
 		},
 	},
 });
