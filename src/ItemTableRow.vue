95f0777aaa8f51dbea769e801850a7aa0ab5cc2a
ginpei <ginpei@gmail.com>
2017-06-11 15:13:00 -0700

    edit name

--- src/ItemTableRow.vue
+++ src/ItemTableRow.vue
@@ -5,6 +5,7 @@
 		</td>
 		<td class="row-name">{{name}}</td>
 		<td class="row-actions">
+			<button @click="edit_onClick">Edit</button>
 			<button @click="delete_onClick">Delete</button>
 		</td>
 	</tr>
@@ -23,6 +24,13 @@ module.exports = {
 	},
 
 	methods: {
+		edit_onClick(event) {
+			const name = window.prompt('Name', this.name);
+			if (name) {
+				this.name = name;
+			}
+		},
+
 		delete_onClick(event) {
 			this.$store.dispatch('removeItems', [this.id]);
 		},
